fe6646d49bb1ed84a3d8b07e7d326c8874040ac9
sumitkute <sumit_kute@yahoo.com>
2019-05-20 07:38:56 +0530

    Add project files.

---
+++ ServiceBusSenderReceiver/Program.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Core;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServiceBusSenderReceiver
+{
+    class Program
+    {
+        // Connection String for the namespace can be obtained from the Azure portal under the 
+        // 'Shared Access policies' section.
+        const string ServiceBusConnectionString = @"Endpoint=sb://testthoughput.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=2VeEGaB+SuEAK4ltSrmBU3zrNLyhSfk8eDcftiLjai0=;";
+        const string QueueName = "clientqueue";
+        static string messageQueueName = string.Empty;
+        static IQueueClient queueClient;
+
+
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName, ReceiveMode.ReceiveAndDelete);
+
+            Console.WriteLine("======================================================");
+            Console.WriteLine("Press ENTER key to exit after receiving all the messages.");
+            Console.WriteLine("======================================================");
+
+            // Register QueueClient's MessageHandler and receive messages in a loop
+            RegisterOnMessageHandlerAndReceiveMessages();
+
+            while (true)
+            { }
+        }
+
+        static void RegisterOnMessageHandlerAndReceiveMessages()
+        {
+            // Configure the MessageHandler Options in terms of exception handling, number of concurrent messages to deliver etc.
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                // Maximum number of Concurrent calls to the callback `ProcessMessagesAsync`, set to 1 for simplicity.
+                // Set it according to how many messages the application wants to process in parallel.
+                MaxConcurrentCalls = 1,
+
+                // Indicates whether MessagePump should automatically complete the messages after returning from User Callback.
+                // False below indicates the Complete will be handled by the User Callback as in `ProcessMessagesAsync` below.
+                AutoComplete = false,
+                  
+                
+            };
+
+            // Register the function that will process messages
+            queueClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+        }
+
+        static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            // Process the message
+            Console.WriteLine($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} Body:{Encoding.UTF8.GetString(message.Body)}");
+
+            dynamic messagebody = JsonConvert.DeserializeObject(Encoding.UTF8.GetString(message.Body));
+            string messageQueueName = messagebody.name;
+
+            // Complete the message so that it is not received again.
+            // This can be done only if the queueClient is created in ReceiveMode.PeekLock mode (which is default).
+            //await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+            await queueClient.CloseAsync();
+            // Note: Use the cancellationToken passed as necessary to determine if the queueClient has already been closed.
+            // If queueClient has already been Closed, you may chose to not call CompleteAsync() or AbandonAsync() etc. calls 
+            // to avoid unnecessary exceptions.
+
+            Console.WriteLine($"Queue name : { messageQueueName }");
+            
+            //long loopcount = 0;
+            //var receiver = new MessageReceiver(ServiceBusConnectionString, messageQueueName, ReceiveMode.ReceiveAndDelete);
+
+            while (true)
+            {
+                QueueOps ops = new QueueOps();
+                //Console.WriteLine($"Sending Message to { messageQueueName }");
+                //await SendMessagesAsync(queueCnt, 50);
+                //IList<Message> receiveMessages = await receiver.ReceiveAsync(50);
+                //Console.WriteLine($"Message Received : { receiveMessages.Count }");
+                await ops.Run(ServiceBusConnectionString, messageQueueName);
+            }
+        }
+
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+            var context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+            Console.WriteLine("Exception context for troubleshooting:");
+            Console.WriteLine($"- Endpoint: {context.Endpoint}");
+            Console.WriteLine($"- Entity Path: {context.EntityPath}");
+            Console.WriteLine($"- Executing Action: {context.Action}");
+            return Task.CompletedTask;
+        }
+
+      
+
+
+    }
+
+}
---
+++ ServiceBusSenderReceiver/QueueOps.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Core;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServiceBusSenderReceiver
+{
+    public class QueueOps
+    {
+        public async Task Run(string ServiceBusConnectionString, string messageQueueName)
+        {
+            var cts = new CancellationTokenSource();
+            var sendTask = SendMessagesAsync(ServiceBusConnectionString, messageQueueName, 1);
+            //var receiver = new MessageReceiver(ServiceBusConnectionString, messageQueueName, ReceiveMode.ReceiveAndDelete);
+            //receiver.OperationTimeout = TimeSpan.FromHours(2);
+            //var receiveTask = ReceiveMessagesAsync(ServiceBusConnectionString, messageQueueName, cts.Token);
+            //var receiveTask = receiver.ReceiveAsync(50);
+
+            await Task.WhenAll(
+                // Task.WhenAny(
+                //    Task.Run(() => Console.ReadKey()),
+                //    //Task.Delay(TimeSpan.FromSeconds(5))
+                //).ContinueWith((t) => cts.Cancel()),
+                sendTask
+                //receiveTask
+                );
+
+            //await receiver.CloseAsync();
+        }
+        async Task SendMessagesAsync(string ServiceBusConnectionString, string messageQueueName, int numberOfMessagesToSend)
+        {
+            IQueueClient queueCnt = new QueueClient(ServiceBusConnectionString, messageQueueName);
+            queueCnt.OperationTimeout = TimeSpan.FromHours(2);
+            IList<Message> messages = new List<Message>();
+            try
+            {
+                for (var i = 0; i < numberOfMessagesToSend; i++)
+                {
+                    // Create a new message to send to the queue
+                    string messageBody = $"Message {i}";
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+                    messages.Add(message);
+                }
+                // Write the body of the message to the console
+                Console.WriteLine($"Sending messages: {messages.Count}");
+
+                // Send the message to the queue
+                await queueCnt.SendAsync(messages);
+
+                await queueCnt.CloseAsync();
+
+                Console.WriteLine($"Messages Send: {messages.Count}");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+        }
+        async Task<dynamic> ReceiveMessagesAsync(string connectionString, string queueName, CancellationToken cancellationToken)
+        {
+            var receiver = new MessageReceiver(connectionString, queueName, ReceiveMode.PeekLock);
+            dynamic queuemetdata = "";
+
+            var doneReceiving = new TaskCompletionSource<bool>();
+            // close the receiver and factory when the CancellationToken fires 
+            cancellationToken.Register(
+                async () =>
+                {
+                    await receiver.CloseAsync();
+                    doneReceiving.SetResult(true);
+                });
+
+            // register the RegisterMessageHandler callback
+            receiver.RegisterMessageHandler(
+                async (message, cancellationToken1) =>
+                {
+                    if (message.Label != null &&
+                        message.ContentType != null &&
+                        //message.Label.Equals("Scientist", StringComparison.InvariantCultureIgnoreCase) &&
+                        message.ContentType.Equals("application/json", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        var body = message.Body;
+
+                        queuemetdata = JsonConvert.DeserializeObject(Encoding.UTF8.GetString(body));
+
+                        lock (Console.Out)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            Console.WriteLine(
+                                "\t\t\t\tMessage received: \n\t\t\t\t\t\tMessageId = {0}, \n\t\t\t\t\t\tSequenceNumber = {1}, \n\t\t\t\t\t\tEnqueuedTimeUtc = {2}," +
+                                "\n\t\t\t\t\t\tExpiresAtUtc = {5}, \n\t\t\t\t\t\tContentType = \"{3}\", \n\t\t\t\t\t\tSize = {4},  \n\t\t\t\t\t\tContent: [ firstName = {6}, name = {7} ]",
+                                message.MessageId,
+                                message.SystemProperties.SequenceNumber,
+                                message.SystemProperties.EnqueuedTimeUtc,
+                                message.ContentType,
+                                message.Size,
+                                message.ExpiresAtUtc,
+                                queuemetdata.name);
+                            Console.ResetColor();
+                        }
+                        await receiver.CompleteAsync(message.SystemProperties.LockToken);
+                    }
+                    else
+                    {
+                        await receiver.DeadLetterAsync(message.SystemProperties.LockToken); //, "ProcessingError", "Don't know what to do with this message");
+                    }
+                },
+                new MessageHandlerOptions((e) => LogMessageHandlerException(e)) { AutoComplete = false, MaxConcurrentCalls = 1 });
+
+            await doneReceiving.Task;
+            return queuemetdata;
+        }
+        Task LogMessageHandlerException(ExceptionReceivedEventArgs e)
+        {
+            Console.WriteLine("Exception: \"{0}\" {0}", e.Exception.Message, e.ExceptionReceivedContext.EntityPath);
+            return Task.CompletedTask;
+        }
+
+    }
+}
